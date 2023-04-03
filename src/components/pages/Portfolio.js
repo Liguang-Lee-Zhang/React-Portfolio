9b6ff4f83b1c1a41666bf493a9c8a10228cb11dd
Liguang Zhang <leezhang97@gmail.com>
2023-04-03 14:56:58 -0400

    added additional project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -30,6 +30,7 @@ export default function Portfolio() {
       <div className="container py-5">
         <h1 className="text-start mb-5">Projects</h1>
         <div className="row row-cols-1 row-cols-md-2 g-4">
+
           <div className="col">
             <div className="card h-100">
               <a></a><img src="./assets/images/codefolio.png" className="card-img-top" alt="Project 1" />
@@ -78,6 +79,34 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
+
+
+          <div className="col">
+            <div className="card h-100">
+              <a></a><img src="./assets/images/cryptocoin.png" className="card-img-top" alt="Project 1" />
+              <div className="card-body">
+                <h3 className="card-title">CryptoCoin</h3>
+                <p className="card-text">Cryptocurrency information</p>
+                <small>Built using REACT, Node, Express, MongoDB, Bootstrap
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="https://cryptocoin.herokuapp.com/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+                      <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
+                      <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
+                    </svg>
+                  </a>
+                  &nbsp;&nbsp;
+                  <a href="https://github.com/vasilyl1/crypto-coin">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg>
+                  </a>
+                </small>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
 
